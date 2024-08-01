a4354ca6d1809f7806b14c8ea1ddfd923120f502
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-08-01 11:30:00 +0530

    Bcrypt Password Encoder

--- SpringSecurityEx/src/main/java/com/telusko/SpringSecurityEx/service/UserService.java
+++ SpringSecurityEx/src/main/java/com/telusko/SpringSecurityEx/service/UserService.java
@@ -3,6 +3,7 @@ package com.telusko.SpringSecurityEx.service;
 import com.telusko.SpringSecurityEx.model.Users;
 import com.telusko.SpringSecurityEx.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,10 @@ public class UserService{
     @Autowired
     private UserRepo userRepo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users registerUser(Users users) {
+        users.setPassword(encoder.encode((users.getPassword())));
         return userRepo.save(users);
     }
 }
